8a8c50506e8551a15eebe75f93991bb8c4b1d821
Pierre Chevin <chevin.pierre.tomas@gmail.com>
2023-08-21 23:59:19 +0200

    new architecture

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Homepage from './Interface/Homepage';
+import Quiz from './Interface/Quiz';
+import Proposal from './Interface/Proposal';
+import Productdetails from './Interface/Productdetails';
+import Categorydetails from './Interface/Categorydetails';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+      return(
+    
+        <div className="App">
+          <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Homepage/>} />
+            <Route path='/quiz' exact element={<Quiz/>} />
+            <Route path='/result' exact element={<Proposal/>} />
+            <Route path='/product details' exact element={<Productdetails/>} />
+            <Route path='/category details' exact element={<Categorydetails/>} />
+            </Routes>
+      </BrowserRouter>
+      </div>
   );
+  
 }
 
 export default App;
